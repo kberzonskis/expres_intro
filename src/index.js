07add83298ec1c95a3942789a57b3f60ed83e4b0
kberzonskis <k.berzonskis@gmail.com>
2025-06-24 21:42:20 +0200

    21*42

--- src/index.js
+++ src/index.js
@@ -1,47 +1,20 @@
 
 import express from 'express'; 
+
+
+import { PageHome } from './pages/PageHome.js';
+import { PageAbout } from './pages/PageAbout.js';
+import { Page404 } from './pages/Page404.js';
+import { PageRegister } from './pages/PageRegister.js';
+
 const app = express()
 const port = 3200; 
 
 
-app.get('/', (req, res) => {
-  
-  res.send('Home content!')
- 
-  
-});
-
-
-app.get('/about', (req, res) => {
-  console.log('about');
-  
-  res.send(`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-</head>
-<body>
-   <h1>SVEIKI DRAUGAI</h1> 
-    <p>Šis yra paprastas HTML dokumentas.</p>
-</body>
-</body>
-</body>
-</html>`)
-});
-
-
-app.get('/*error', (req, res) => {
-  console.log('404 error');
-  
-  res.send('page not found 404 error')
-});
-
-
-
-
+app.get('/', (req, res) => res.send(new PageHome().render()));
+app.get('/about', (req, res) => res.send(new PageAbout().render()));
+app.get('/register', (req, res) => res.send(new PageRegister().render()));  
+app.get('/*error', (req, res) => res.send(new Page404().render()));   
 
 
 app.listen(port, () => {
