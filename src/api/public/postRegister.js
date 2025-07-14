b0fa7e52101fcc8b06480ece1cfddeb84aac1c9d
kberzonskis <k.berzonskis@gmail.com>
2025-07-14 11:32:00 +0200

    11*31

--- src/api/public/postRegister.js
+++ src/api/public/postRegister.js
@@ -1,65 +1,86 @@
-/*import { IsValid } from "../../lib/IsValid";
-
-export function postRegister(req,res) { 
-    const [err,msg] = IsValid.fields(req.body, { 
-       username: 'username', 
-    password: 'password',
-    })
-
-    if (err) { return res.json ({
-     status: 'error', 
-     msg:msg,
-
-    }) }
-} 
-
-
-{   console.log(req.body);
-
-    
-    
-    return res.json( {
-    status: 'success',
-    msg: 'Sekminga registracija',
-}
-
-);
-}
-*/ 
-
-
-
-
+import { response } from "express";
+import { connection } from "../../db.js"
 import { IsValid } from "../../lib/IsValid.js";
-import { connection } from "../../db.js";
-
+import { randomString } from "../../lib/randomString.js";
+import { hash } from "../../lib/hash.js";
 export async function postRegister(req, res) {
     const [err, msg] = IsValid.fields(req.body, {
         username: 'username',
         email: 'email',
         password: 'password',
-       
     });
-
+ 
     if (err) {
         return res.json({
             status: 'error',
             msg: msg,
         });
     }
-const { username, email, password} = req.body;
 
-try { const sql = 'INSERT INTO users(username, email, password) VALUES (?,?,?);';
-    const response = await connection.execute(sql,[username, email, password])
-  console.log(response);
+   
+    const { username, email, password } = req.body;
+ 
+
+    try {
+        const sql = `SELECT * FROM users WHERE username = ? OR email = ?;`;
+        const [response] = await connection.execute(sql, [username, email]);
+ 
+                if (response.length>0) {
+            return res.status(400).json({
+                status: 'error',
+                msg: 'Jau toks vartotojas uzregistruotas.. bandyti rinktis kita',
+            });
+        }
+            
+    }
+    
+    catch (error) {
+          console.log(error) 
+            return res.status(500). json ({ 
+                status: 'error', 
+                msg: 'Serverio klaida',
+            })
+     } 
+    
+    
+    
+   const salt = randomString(10);
+   const passwordHash = hash(password+salt); 
+ 
+    try {
+        const sql = `INSERT INTO users (username, email,salt, password_hash) VALUES (?,?, ?, ?);`;
+        const [response] = await connection.execute(sql, [ username, email, password,passwordHash]);
+ 
+                if (response.affectedRows !== 1) {
+            return res.status(500).json({
+                status: 'error',
+                msg: 'Serverio klaida',
+            });
+        }
+
+    }
+    
+    catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') { 
+            return res.status(500). json ({ 
+                status: 'error', 
+                msg: 'kartojasi irasas'
+            })
+     }      
   
-
-} catch (error) {console.log(error) }
-   ;
-      
-
-    return res.json({
+    console.log(error);
+    return res.status(500).json({
         status: 'success',
-        msg: 'Sekminga registracija',
-    });
-}  
+        msg: 'serviso klaida',
+    })
+    }
+
+
+    return res.status(201).json({
+        status: 'success',
+        msg: 'Registration successful',
+    })
+
+
+
+}
---
+++ src/lib/hash.js
@@ -0,0 +1,14 @@
+import { log } from "console";
+import { createHash } from "crypto";
+
+export function hash(text) { 
+try { 
+    return createHash('sha512').update(text).digest('hex');
+}
+catch (error) { 
+    return ''; 
+}
+
+}  
+
+console.log ('---', hash())
