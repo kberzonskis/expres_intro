8c58da4edd4e82fb16f71d987759c91366d81247
kberzonskis <k.berzonskis@gmail.com>
2025-07-14 14:53:20 +0200

    14*53

---
+++ src/api/public/postLogin.js
@@ -0,0 +1,85 @@
+import { response } from "express";
+import { connection } from "../../db.js"
+import { IsValid } from "../../lib/IsValid.js";
+import { randomString } from "../../lib/randomString.js";
+import { hash } from "../../lib/hash.js";
+export async function postRegister(req, res) {
+    const [err, msg] = IsValid.fields(req.body, {
+        username: 'noEmptyString',
+        password: 'password',
+    });
+ 
+    if (err) {
+        return res.json({
+            status: 'error',
+            msg: msg,
+        });
+    }
+
+   
+    const { username, password } = req.body;
+ 
+
+    try {
+        const sql = `SELECT * FROM users WHERE username = ? OR email = ?;`;
+        const [response] = await connection.execute(sql, [username, email]);
+ 
+                if (response.length>0) {
+            return res.status(400).json({
+                status: 'error',
+                msg: 'Jau toks vartotojas uzregistruotas.. bandyti rinktis kita',
+            });
+        }
+            
+    }
+    
+    catch (error) {
+          console.log(error) 
+            return res.status(500). json ({ 
+                status: 'error', 
+                msg: 'Serverio klaida',
+            })
+     } 
+    
+    
+    
+   const salt = randomString(10);
+   const passwordHash = hash(password+salt); 
+ 
+    try {
+        const sql = `INSERT INTO users (username, email,salt, password_hash) VALUES (?,?, ?, ?);`;
+        const [response] = await connection.execute(sql, [ username, email, password,passwordHash]);
+ 
+                if (response.affectedRows !== 1) {
+            return res.status(500).json({
+                status: 'error',
+                msg: 'Serverio klaida',
+            });
+        }
+
+    }
+    
+    catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') { 
+            return res.status(500). json ({ 
+                status: 'error', 
+                msg: 'kartojasi irasas'
+            })
+     }      
+  
+    console.log(error);
+    return res.status(500).json({
+        status: 'success',
+        msg: 'serviso klaida',
+    })
+    }
+
+
+    return res.status(201).json({
+        status: 'success',
+        msg: 'Registration successful',
+    })
+
+
+
+}
